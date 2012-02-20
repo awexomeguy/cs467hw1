b3f65ddaf19e8054a7045c5ae0fd6751273507cb
cljvf9 <cljvf9@rc04xcs213.managed.mst.edu>
2012-02-20 16:33:23 -0600

    implemented the call function for the PointerTree class

--- PointerTree.java
+++ PointerTree.java
@@ -69,4 +69,54 @@ public class PointerTree extends TreeStructure
 			}
 		}
 	}
+	
+	// given a starting leaf node, will find the node containing the phone
+	// number in question
+	public DefaultMutableTreeNode call(DefaultMutableTreeNode startingNode, int phoneNumber)
+	{
+		System.out.println("starting call at: " + startingNode);
+		// first search the same cell, if found then just return the local cell
+		ArrayList localNumbers = (ArrayList)startingNode.getUserObject();
+		if(localNumbers.contains(phoneNumber))
+			return startingNode;
+		
+		DefaultMutableTreeNode currentNode = startingNode;
+		HashMap map;
+		
+		// not found locally, start moving up the tree until an entry
+		// for the requested number is found or we get to the root
+		do
+		{
+			currentNode = (DefaultMutableTreeNode)currentNode.getParent();
+			map = (HashMap)currentNode.getUserObject();
+			System.out.println("moving up to " + currentNode);
+		}while(!map.containsKey(phoneNumber) && currentNode != getTop());
+		
+		// if the number is still not found, we assume this means we hit
+		// the top of the tree and return failure
+		if(!map.containsKey(phoneNumber))
+			return null;
+		else
+		{
+			// if we are here, it means we have found the number and can
+			// start following pointers until we get to the correct cell
+			while(!currentNode.isLeaf())
+			{
+				map = (HashMap)currentNode.getUserObject();
+				currentNode = (DefaultMutableTreeNode)currentNode.getChildAt((Integer)map.get(phoneNumber));
+				System.out.println("moving down to " + currentNode);
+			}
+			
+			// just to make sure, check the resulting node to see if it
+			// has the phone number we want (should always happen unless
+			// something outside this object modified the tree)
+			localNumbers = (ArrayList)currentNode.getUserObject();
+			if(localNumbers.contains(phoneNumber))
+				return currentNode;
+		}
+		
+		// this is just because java wants a return statement at the
+		// end of a function
+		return null;
+	}
 }
